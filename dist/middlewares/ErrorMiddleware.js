bb512db279c40e5d4001e32b3584812da8d5c948
Sypha999 <datonarinze@gmail.com>
2026-05-30 10:22:27 +0100

    First commit

--- dist/middlewares/ErrorMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error, req, res, next) => {
-    res.status(error.statusCode || 500).json({
-        message: error.message || 'Server Error'
-    });
-};
-exports.errorHandler = errorHandler;
